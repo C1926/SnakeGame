01844685ef0153fb59bf78b18bb812fa0ebd2a05
C1926 <2539936496@qq.com>
2020-04-02 12:34:47 +0800

    贪吃蛇 第三版

--- com/snake/game/bg/Gsnake.java
+++ com/snake/game/bg/Gsnake.java
@@ -2,8 +2,11 @@ package com.snake.game.bg;
 
 import sun.plugin.viewer.context.AxBridgeAppletContext;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * @author swx
@@ -11,12 +14,28 @@ import java.awt.*;
  * <p> creat by 2020/3/31 on 17:09
  */
 public class Gsnake {
-    private   static ImageIcon icon= new ImageIcon("./resource/img/icon.jpg");
+    private static ImageIcon icon;
+    private static Gsnake gsnake=new Gsnake();
+
+    public static Gsnake getGsnake() {
+        return gsnake;
+    }
+
+    public  ImageIcon loadIcon() {
+        InputStream is=this.getClass().getClassLoader().getResourceAsStream("img/icon.jpg");
+        try {
+            icon=new ImageIcon(ImageIO.read(is));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return icon;
+    }
     public static void main(String[] args) {
+        gsnake=getGsnake();
         JFrame frame = new JFrame();//创建窗口
         frame.setTitle("Snake Game");
         //设置窗口图标
-        frame.setIconImage(icon.getImage());
+        frame.setIconImage(gsnake.loadIcon().getImage());
         frame.setBounds(600,200,900,720);//在坐标x:600,y:200出现。大小为900*720
         frame.setResizable(false);//不允许调正窗口大小
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //点击×即关闭窗口
